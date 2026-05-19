2f58745b5bda9562e402e09e382cd119e8286032
Ana <37382332+AnAndric@users.noreply.github.com>
2026-05-19 21:51:21 +0200

    commit

--- events-be/src/main/java/com/sokoj/events/config/CorsConfig.java
+++ events-be/src/main/java/com/sokoj/events/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     private final String[] allowedOrigins;
 
     public CorsConfig(
-            @Value("${app.cors.allowed-origins:http://localhost:3000,http://127.0.0.1:3000}") String allowedOrigins
+            @Value("${app.cors.allowed-origins:http://localhost:3000,http://127.0.0.1:3000,https://frontend-production-a87d.up.railway.app/}") String allowedOrigins
     ) {
         this.allowedOrigins = Arrays.stream(allowedOrigins.split(","))
                 .map(String::trim)
